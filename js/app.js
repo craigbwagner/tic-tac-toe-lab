4b96f75c165f2a947c8228a2329c34c351a0686e
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-12 20:47:04 -0400

    added function and winningCombos constant to complete steps 4 and 5

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
 /*---------------------------- Variables (state) ----------------------------*/
 let board;
 let turn;
@@ -18,7 +27,26 @@ function init() {
 	render();
 }
 
-function render() {}
-/*----------------------------- Event Listeners -----------------------------*/
+function render() {
+	updateBoard();
+	updateMessage();
+}
 
+function updateBoard() {
+	board.forEach((element, index) => {
+		squareEls[index].textContent = element;
+	});
+	console.log(squareEls);
+}
+
+function updateMessage() {
+	if (winner === false && tie === false) {
+		messageEl.textContent = `It is ${turn}'s turn`;
+	} else if (winner === false && tie === true) {
+		messageEl.textContent = "It's a tie!";
+	} else {
+		messageEl.textContent = `Player ${turn} has won!`;
+	}
+}
+/*----------------------------- Event Listeners -----------------------------*/
 init();
