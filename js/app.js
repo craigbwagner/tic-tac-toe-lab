b2e73c94e44b2455f4e4850f0564ac14bc232751
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-12 20:16:21 -0400

    completed step 2

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ let turn;
 let winner = false;
 let tie = false;
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll('.sqr');
+const messageEl = document.querySelector('#message');
 
 /*-------------------------------- Functions --------------------------------*/
 
